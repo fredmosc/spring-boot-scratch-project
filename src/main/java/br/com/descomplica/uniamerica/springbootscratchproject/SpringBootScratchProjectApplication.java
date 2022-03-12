8ba4a3a447a952635a6bac5a57f8bf36766d7c36
FrederickNazarioMosc <fm.webdeveloper@gmail.com>
2022-03-12 17:37:42 -0300

    Initial Commit

---
+++ src/main/java/br/com/descomplica/uniamerica/springbootscratchproject/SpringBootScratchProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.descomplica.uniamerica.springbootscratchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScratchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootScratchProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/descomplica/uniamerica/springbootscratchproject/SpringBootScratchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.descomplica.uniamerica.springbootscratchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootScratchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
